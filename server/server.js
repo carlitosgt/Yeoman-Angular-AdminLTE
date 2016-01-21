c38a0c305c7a44af064155650a62c3480f05e0da
DucPT <ducpt1012@gmail.com>
2016-01-22 01:10:31 +0700

    Signed-off-by: DucPT <ducpt1012@gmail.com>

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ app.use(express.static(path.join(__dirname,'/../dist')));
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/news_api', function(req, res){
 
 	url = 'https://tinhte.vn';
