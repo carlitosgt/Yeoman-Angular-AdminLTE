6371742c5cb1359dca2300b3f1b3d7a8c8574da1
ducpt1012@gmail.com <ducpt1012@gmail.com>
2016-02-09 21:42:10 +0700

    Signed-off-by: ducpt1012@gmail.com <ducpt1012@gmail.com>

--- app/scripts/controllers/music.js
+++ app/scripts/controllers/music.js
@@ -8,7 +8,9 @@
  * Controller of the yeomanAdminLteApp
  */
  angular.module('yeomanAdminLteApp')
- .controller('MusicCtrl', function ( $scope,$http) {
+ .controller('MusicCtrl', function ($rootScope,$scope,$http) {
+ 	$rootScope.moduleName ="Music";
+ 	$rootScope.moduleNameDetails ="Get Album Mp3.Zing - Nhaccuatui";
  	$scope.getAlbum = function() {
  		$http.get("/api/getAlbumMp3?urlAlbum=" + $scope.urlAlbum)
  		.success(function(data) {
