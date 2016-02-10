3d140f21a708a92b75ef21844ab1cbb61f9caf5e
ducpt1012@gmail.com <ducpt1012@gmail.com>
2016-02-10 18:44:10 +0700

    Signed-off-by: ducpt1012@gmail.com <ducpt1012@gmail.com>

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,14 +1,14 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name yeomanAdminLteApp.controller:ProfileCtrl
- * @description
- * # ProfileCtrl
- * Controller of the yeomanAdminLteApp
- */
+* @ngdoc function
+* @name yeomanAdminLteApp.controller:ProfileCtrl
+* @description
+* # ProfileCtrl
+* Controller of the yeomanAdminLteApp
+*/
 angular.module('yeomanAdminLteApp')
-  .controller('ProfileCtrl', function ($rootScope) {
-    $rootScope.moduleName ="Profile";
- 	$rootScope.moduleNameDetails ="This is the profile view";
-  });
+.controller('ProfileCtrl', function ($rootScope) {
+  $rootScope.moduleName ="Profile";
+  $rootScope.moduleNameDetails ="This is the profile view";
+});
